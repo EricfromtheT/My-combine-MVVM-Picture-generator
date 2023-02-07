0995b6bba94e9b27b462e3bed3a9bc368f1338f8
Eric Kong <33079359a@gmail.com>
2023-02-07 23:37:44 +0800

    Initial Commit

---
+++ My combine AI picture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My combine AI picture
+//
+//  Created by 孔令傑 on 2023/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
